196e77e1a27db131b0754d36376048d49e1fd81c
Qarani-m <emqarani@gmail.com>
2026-04-30 20:16:33 +0300

    feat: implement profile-based CORS configurations and update logging and management endpoints in application-dev.yml

--- src/main/java/com/microservices/gateway/config/CorsConfig.java
+++ src/main/java/com/microservices/gateway/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.microservices.gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,9 +12,28 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+        @Value("${sping.profiles.active}")
+        private String profile;
+
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
+
+        if (profile.equals("dev")) {
+            config.setAllowedOrigins(List.of(
+                    "http://localhost:5173",
+                    "http://localhost:5174"));
+        } else if (profile.equals("staging")) {
+            config.setAllowedOrigins(List.of(
+                    "http://localhost:5173",
+                    "http://localhost:5174",
+                    "http://139.144.169.155:5173",
+                    "http://139.144.169.155:5174"));
+        } else if (profile.equals("prod")) {
+            config.setAllowedOrigins(List.of(
+                    "http://139.144.169.155:5173",
+                    "http://139.144.169.155:5174"));
+        }
 
         // ✅ FIX: remove "/login" — origins must NOT include paths
         config.setAllowedOrigins(List.of(
